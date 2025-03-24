bc78ae6e94cc2f139eec926a4f7a7870e08a0772
zayn <98650340+DSYZayn@users.noreply.github.com>
2025-03-24 11:58:34 +0800

    feat: remove tests before build in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import GopeedPolyfillPlugin from 'gopeed-polyfill-webpack-plugin';
-import { execSync } from 'child_process';
 
 const __dirname = fileURLToPath(import.meta.url);
 
@@ -12,6 +11,7 @@ export default (_, argv) => ({
     path: path.resolve(__dirname, '../dist'),
   },
   devtool: argv.mode === 'production' ? undefined : false,
+  plugins: [new GopeedPolyfillPlugin()],
   module: {
     rules: [
       {
@@ -22,20 +22,4 @@ export default (_, argv) => ({
       },
     ],
   },
-  plugins: [
-    new GopeedPolyfillPlugin(),
-    {
-      apply: (compiler) => {
-        compiler.hooks.beforeCompile.tap('RunTestsBeforeBuild', () => {
-          try {
-            console.log('Running tests before build...');
-            execSync('npm run test', { stdio: 'inherit' });
-          } catch (err) {
-            console.error('Tests failed:', err);
-            process.exit(1);
-          }
-        });
-      },
-    },
-  ],
 });
